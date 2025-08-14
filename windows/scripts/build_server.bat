9f9814cef2b87c39af6e8afef631aaaf3b872080
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-08-14 10:26:07 -0500

    use arguments to merge similar windows scripts

--- windows/scripts/build_server.bat
+++ windows/scripts/build_server.bat
@@ -1,12 +1,18 @@
 @echo off
-call .\clean.bat
+REM Run from pankosmia\[this-repo's-name]\windows\scripts directory in powershell or command by:  .\build_server.bat
+REM Git hub actions use this with optional arguments of: .\build_server.bat -c
+REM because it is a brand new clean environment without need for any prior build to be removed.
+
+IF NOT "%~1"=="-c" (
+  call .\clean.bat
+)
 if not exist ..\..\local_server\target\release\local_server.exe (
-    echo "Building local server"
+    echo "Building local server..."
     cd ..\..\local_server
     cargo build --release
     cd ..\windows\scripts
 )
 if not exist ..\build (
-  echo "Assembling build environment"
+  echo "Assembling build environment..."
   node build.js
 )
