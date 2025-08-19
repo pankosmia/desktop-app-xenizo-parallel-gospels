d6c53dd5e923a2ab2655d38fb09b7d028f917638
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-08-19 16:13:35 -0500

    Add macos architecture to local buindle zip

--- macos/scripts/bundle_zip.zsh
+++ macos/scripts/bundle_zip.zsh
@@ -99,17 +99,15 @@ if ! [[ $isGHA =~ ^(-g) ]]; then
   node build.js
 fi
 
-if [[ $isGHA =~ ^(-g) ]]; then
-  # Detect current CPU architecture
-  CPU_ARCH=$(uname -m)
-  if [ "$CPU_ARCH" = "x86_64" ]; then
-      CPU_ARCH="intel64"
-  elif [ "$CPU_ARCH" = "arm64" ]; then
-      CPU_ARCH="arm64"
-  else
-      echo "Error: Unsupported CPU architecture: $CPU_ARCH, default to intel64"
-      CPU_ARCH="intel64"
-  fi
+# Detect current CPU architecture
+CPU_ARCH=$(uname -m)
+if [ "$CPU_ARCH" = "x86_64" ]; then
+    CPU_ARCH="intel64"
+elif [ "$CPU_ARCH" = "arm64" ]; then
+    CPU_ARCH="arm64"
+else
+    echo "Error: Unsupported CPU architecture: $CPU_ARCH, default to intel64"
+    CPU_ARCH="intel64"
 fi
 
 echo
@@ -128,5 +126,5 @@ APP_NAME=${APP_NAME:l}
 APP_NAME=${APP_NAME// /-}
 # Make executable and zip
 chmod +x $APP_NAME.zsh
-zip -r ../../releases/macos/$APP_NAME-macos-$APP_VERSION.zip * -x post_install_script.sh appLauncher.sh
+zip -r ../../releases/macos/$APP_NAME-macos-$CPU_ARCH-$APP_VERSION.zip * -x post_install_script.sh appLauncher.sh @ &> /dev/null
 cd ../scripts
